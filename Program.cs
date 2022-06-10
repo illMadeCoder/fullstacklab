e0dc7a6fc977651f78fa189def5a0c56e9bc4568
illMadeCoder <illMadeCoder@gmail.com>
2022-06-10 19:31:16 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var app = builder.Build();
 app.MapGet("/", 
   (HttpContext context) => {
     context.Response.ContentType = "text/html";
-    return File.ReadAllText(Path.Combine("~", "ClientApp", "public", "index.html"));
+    return File.ReadAllText(@"../ClientApp/public/index.html"));
   }
 );
 
