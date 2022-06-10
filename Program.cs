8a00764c2799969dfb74f987ad58cafc0d9a1c59
illMadeCoder <illMadeCoder@gmail.com>
2022-06-10 19:24:17 -0400

    debug2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+string baseURL = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).ToString()).ToString()).ToString()).ToString();
+
 app.MapGet("/", 
   (HttpContext context) => {
     context.Response.ContentType = "text/html";
-    return File.ReadAllText(Path.Combine(".", "ClientApp", "public", "index.html"));
+    return File.ReadAllText(Path.Combine(baseURL, "ClientApp", "public", "index.html"));
   }
 );
 
 app.MapGet("/info", () => "info");
-
+Console.WriteLine(baseURL);
 Console.WriteLine(Path.GetFullPath(Path.Combine(".", "ClientApp", "public", "index.html")));
 Console.WriteLine(File.Exists(Path.Combine(".", "ClientApp", "public", "index.html")));
 
