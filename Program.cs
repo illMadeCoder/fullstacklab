d9c792a8238287b6296366a927689f9354ed277e
illMadeCoder <illMadeCoder@gmail.com>
2022-06-10 19:12:27 -0400

    debug

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ var app = builder.Build();
 
 app.MapGet("/", 
   (HttpContext context) => {
-    context.Response.ContentType ="text/html";
+    context.Response.ContentType = "text/html";
     return File.ReadAllText(Path.Combine(".", "ClientApp", "public", "index.html"));
   }
 );
 
 app.MapGet("/info", () => "info");
 
-Console.WriteLine(Path.Combine(".", "ClientApp", "public", "index.html"));
+Console.WriteLine(Path.GetFullPath(Path.Combine(".", "ClientApp", "public", "index.html")));
 Console.WriteLine(File.Exists(Path.Combine(".", "ClientApp", "public", "index.html")));
 
 app.Run();
