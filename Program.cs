66431b8da7132114f1422bd8237257fe7777933f
illMadeCoder <illMadeCoder@gmail.com>
2022-06-11 11:33:03 -0400

    debug3

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var publicPath = Path.GetFullPath(Path.Combine(".", "ClientApp", "public"));
+var indexPath = Path.Combine(publicPath, "index.html");
+if (File.Exists(indexPath) == false)
+{
+  throw new Exception($"Public File Path {publicPath} not found");
+}
 
-app.MapGet("/", 
-  (HttpContext context) => {
-    context.Response.ContentType = "text/html";
-    return File.ReadAllText(@"../ClientApp/public/index.html"));
-  }
-);
+
+// app.MapGet("/", 
+//   (HttpContext context) => {
+//     context.Response.ContentType = "text/html";
+//     return File.ReadAllText(indexPath));
+//   }
+// );
 
 app.MapGet("/info", () => "info");
-Console.WriteLine(Path.GetFullPath(Path.Combine(".", "ClientApp", "public", "index.html")));
-Console.WriteLine(File.Exists(Path.Combine(".", "ClientApp", "public", "index.html")));
 
 app.Run();
